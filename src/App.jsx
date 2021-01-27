754bf6d8924c1893a229375da873c12a913648c4
biwenliang <biwenliang@newrank.cn>
2021-01-27 10:58:50 +0800

    feat：配置路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,28 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
+import { HashRouter as Router, Link, Switch } from 'react-router-dom'
 import './App.css'
 
+function About() {
+  return (
+    <div>About</div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Switch>
+      <Router exact path="/">
+        <Link to={{
+          pathname: '/about'
+        }}>
+          点我去关于
+        </Link>
+      </Router>
+      <Router path="/about">
+        <About />
+      </Router>      
+    </Switch>
   )
 }
 
