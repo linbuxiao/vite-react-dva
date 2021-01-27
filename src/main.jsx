1527472f13faac28a4f95d32ad012744a64b1e0d
biwenliang <biwenliang@newrank.cn>
2021-01-27 16:43:48 +0800

    进度40%，挖个坑先。

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from 'react';
+import { Router, Route, Switch } from 'dva/router';
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import './index.css'
+import dva from 'dva'
+import { createHashHistory } from 'history';
+const history = createHashHistory()
+const app = dva({history})
+
+function RouterConfig({ history }) {
+ 
+  return (
+    <Router history={history}>
+      <Switch>
+        <Router path="/" exact component={App}/>
+      </Switch>
+    </Router>
+  );
+}
+
+app.router(RouterConfig)
+app.start('#root');
+
+ 
